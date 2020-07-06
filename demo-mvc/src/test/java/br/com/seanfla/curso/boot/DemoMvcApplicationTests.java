a1e39686f9d95d562b4800a0df211b16d653d5ce
fseverino1981 <fasvb6@gmail.com>
2020-07-06 11:32:08 -0300

    First version of application

---
+++ demo-mvc/src/test/java/br/com/seanfla/curso/boot/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.seanfla.curso.boot;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
